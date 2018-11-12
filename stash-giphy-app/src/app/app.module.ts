cc9495eeb0a69e675ad81bf7468cfb3dff8e2b50
christinacannito <christinacannito@gmail.com>
2018-11-12 16:50:03 -0500

    main components createted

--- stash-giphy-app/src/app/app.module.ts
+++ stash-giphy-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { ResultsComponent } from './results/results.component';
+import { SearchDirective } from './search/search.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavoritesComponent,
+    ResultsComponent,
+    SearchDirective
   ],
   imports: [
     BrowserModule,
