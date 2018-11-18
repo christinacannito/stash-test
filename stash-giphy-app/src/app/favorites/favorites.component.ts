5eec1fddd14cc342d275bfabae6b80e76e47328e
Christina Cannito <christinacannito@Christinas-MacBook-Pro.local>
2018-11-18 08:42:42 -0800

    took out comments and added empty state to favorites

--- stash-giphy-app/src/app/favorites/favorites.component.ts
+++ stash-giphy-app/src/app/favorites/favorites.component.ts
@@ -17,29 +17,18 @@ export class FavoritesComponent implements OnInit {
   }
 
   showFavoriteItems = () => {
-    // returns what was in local storage
     this.allFavorites = this.storage.get(this.localStorageKey)
     console.log('all favorites: ', this.allFavorites)
     return this.allFavorites;
   }
 
   deleteFavorite = (favoriteObject: object) => {
-    // favoriteObject is stored in an array 
-    // do index of and then remove that index from the array
     let indexFavorite = this.allFavorites.indexOf(favoriteObject)
     console.log('found index of favorite: ', indexFavorite)
     console.log('his.allFavorites[indexFavorite]: ', this.allFavorites[indexFavorite])
-    // reset the array 
     this.allFavorites.splice(indexFavorite, 1)
-    this.storage.set(this.localStorageKey, this.allFavorites); // you have to reset localstorage or it save the information
-    let deletedSign = document.getElementById('deleted');
-    deletedSign.classList.add('active')
-    setTimeout(function() {
-      deletedSign.classList.remove('active')
-    }, 1000)
+    this.storage.set(this.localStorageKey, this.allFavorites);
     return  this.allFavorites;
-    // this.storage.remove(this.localStorageKey) // remove the individual item
-    // localStorage.removeItem(this.allFavorites[indexFavorite]);
   }
 
 }
